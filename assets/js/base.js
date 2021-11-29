df2405c81006d41bd0b2a272c8dd765328e0b970
LeoCode12 <leonardo200043@gmail.com>
2021-11-28 23:19:33 -0600

    ADD exercises Class Four

---
+++ assets/js/base.js
@@ -0,0 +1,56 @@
+// Exercises
+
+
+/*
+	Pedir al usuario una palabra
+	Devolver la palabra capitalizada
+	p.ej. 'galAxiA'
+	Ouput -> 'Galaxia'
+*/
+const wordUser = prompt('Dame una oración').trim().toLowerCase()
+var wordCap = wordUser.substring(0,1).toUpperCase() + wordUser.substring(1)
+console.log(wordCap)
+
+/*
+	Pedir al usuario una oración (permitir espacios)
+	Capitalizar cada una de las palabras de la oración
+	p.ej. 'En una galaxia muy muy lejana'
+	Ouput -> 'En Una Galaxia Muy Muy Lejana'
+*/
+
+
+const word = prompt('Dame una oración').trim().toLowerCase()
+let wordCapitalize = ''
+// en una galaxia
+for(i = 0; i < word.length; i++){
+    if(word[i] == ' '){
+        wordCapitalize += ' ' + word[i + 1].toUpperCase()
+        i = i + 1
+    } else {
+        wordCapitalize = wordCapitalize + word[i]
+    }
+}
+// en Una Galaxia
+wordCapitalize = wordCapitalize.substring(0,1).toUpperCase() + wordCapitalize.substring(1)
+// En Una Galaxia
+console.log(wordCapitalize)
+
+
+
+/*
+	Pedir al usuario cuantas materias tiene actualmente
+	Pedir al usuario la calificacion de cada una de esas N materias
+	Arrojar el promedio en un alert
+*/
+
+let totalSubjects = parseInt( prompt('Total de materias') )
+let total = 0, average = 0, counter = 1;
+
+while(counter <= totalSubjects){
+    total = total + parseInt(prompt(`Calificación de la materia ${counter}: `))
+    counter++
+}
+
+average = total / totalSubjects
+alert(`El promedio es: ${average}`)
+
---
+++ assets/js/homework.js
@@ -0,0 +1,79 @@
+/** 
+ * Ejercicio 1
+ * imprimir en consola todos los numeros pares entre 1 y 100
+ * -> 2
+ * -> 4
+ * -> 100
+*/
+
+// for (let i = 1; i <= 100; i++)
+//   if (i % 2 == 0) console.log(i);
+
+/**
+ * Ejercicio 2
+ * Pedir al usuario un string
+ * Invertir el orden e imprimir en consola
+ * Entrada: "reverse"
+ * Salida: -> esrever
+ * hint: https://www.w3schools.com/jsref/jsref_length_string.asp
+*/
+
+// var wordUser = prompt('dame una palabra')
+// var wordReverse = ''
+// let j = wordUser.length-1 // 4 - 1 = 3
+// for (let i = 0; i < wordUser.length; i++) {
+//   wordReverse += wordUser[j].toLowerCase()
+//   j--
+// }
+// console.log(wordReverse);
+
+/**
+ *  word= H o l a     wordReverse = a l o H
+ *  i =   0 1 2 3     j =           3 2 1 0
+ *  j =   3 2 1 0     i =           0 1 2 3
+ */
+
+/**
+ * Ejercicio 3 
+ * Declarar un string de al menos 2 palabras
+ * Imprimir en consola el string separado por guiones en cada palabra
+ * "hola koders"
+ * "hola-koders"
+ * hint: https://www.w3schools.com/jsref/jsref_replace.asp
+ */
+
+// let wordNew = ''
+// for (let i = 0; i < wordUser.length; i++) {
+  
+//   if (wordUser[i] === ' ') {
+//     wordNew += "-"
+//   }
+//   wordNew += wordUser[i].trim()
+// }
+// console.log(wordNew);
+
+/**
+ * Ejercicio 4
+ * Declarar un string con un correo electrónico
+ * Enmascarar con "*" el bloque del usuario
+ * "jorgec@kodemia.mx"
+ * "*****@kodemia.mx"
+ * 
+ * hint: https://www.w3schools.com/jsref/jsref_slice_string.asp
+ * hint: https://www.w3schools.com/jsref/jsref_indexof.asp
+ */
+
+let email = prompt('Correo Electronico')
+emailMask = ''
+let j
+for (let i = 0; i < email.length; i++) {
+  if (email[i] == '@') {
+    j = email.slice(0, i).length
+    while (j > 0) {
+      emailMask += '*'
+      j--
+    }
+    emailMask += email.slice(i, email.length)
+  }
+}
+console.log(emailMask);
