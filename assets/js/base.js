d83f1b6033f255be429c9c5c65ac8d657aa87e56
LeoCode12 <leonardo200043@gmail.com>
2021-12-01 01:07:46 -0600

    ADD exercises Class Fiver

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,8 +1,52 @@
-const sumaValue = (number, sumaValues = 0) =>{
-	for (let i = 0; i < number.length; i++) {
-		sumaValues += parseInt(number[i])
-	}
-	return sumaValues
+
+
+// console.log('funciones')
+
+// funcion sin parametros
+function suma() {
+	return 2 +  2
 }
-let number = prompt(`dame un numero`)
-console.log(sumaValue(number));
+
+// funcion con parametros
+function toCelsius(fahrenheit) {
+	let celsiusGrades = (5 / 9) * (fahrenheit - 32)	
+
+	// retorno de la funcion
+	return celsiusGrades
+}
+
+// let celsiusGradesReturned = toCelsius(100)
+// console.log(celsiusGradesReturned)
+
+/**
+ * Funcion que pida el nombre al usuario
+ * y lo imprima en consola
+ */
+
+function getName() {
+	let nameUser = prompt('Dame tu nombre', 'jorge')
+	console.log(nameUser)
+}
+// getName()
+
+/**
+ * Funcion que reciba una palabra
+ * y la retorne al revés
+ */
+
+function stringReverse (stringToReverse){
+	// hola
+    let stringReversed = ''
+    for(let i = stringToReverse.length - 1; i >= 0 ; i--) {
+        stringReversed += stringToReverse[i]
+    }
+    return stringReversed
+}
+
+
+function sumNumbers(a, b = 5) {
+	let suma = a + b
+	return suma
+}
+
+let resultado = sumNumbers(10)
